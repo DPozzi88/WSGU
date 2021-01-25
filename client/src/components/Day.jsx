b1dd6b322c945811e928c9cb61ff027a9487a939
Dario Pozzi <dariopozzi3@gmail.com>
2021-01-25 07:04:58 -0500

    impostato calendario con funzione map

--- client/src/components/Day.jsx
+++ client/src/components/Day.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+const days = ["1", "2", "3", "4", "5", "6"];
+const riordino = ["🦁", "🦁", "🦁", "🦁", "🦁", "🦁"];
+const yoga = ["🧘🏼‍♂️", "🧘🏼‍♂️", "🧘🏼‍♂️", "🧘🏼‍♂️", "🧘🏼‍♂️", "🧘🏼‍♂️"];
+const coding = ["👨‍💻", "👨‍💻", "👨‍💻", "👨‍💻", "👨‍💻", "👨‍💻"];
+
 function Day(props) {
   return (
-    <tr>
-      <td>{props.day}</td>
-      <td>{props.row1}</td>
-      <td>{props.row2}</td>
-      <td>{props.row3}</td>
-    </tr>
+    <div>
+      <table>
+        <tr>
+          <td>{props.uno}</td>
+          <td>
+            <button>{props.uno}</button>
+          </td>
+          <td>
+            <button>{props.due}</button>
+          </td>
+        </tr>
+      </table>
+    </div>
   );
 }
 
--- server.js
+++ server.js
@@ -18,37 +18,74 @@ const port = 5000;
 
 app.listen(port, () => console.log("Server started on port " + port));
 
-const taskSchema = new mongoose.Schema({
-  giorno: {
-    type: Number,
-    min: 1,
-    max: 31,
+const streakSchema = new mongoose.Schema({
+  //forse "mongoose." si puà ometterE?" https://mongoosejs.com/docs/schematypes.html#arrays
+  day: {
+    type: String,
   },
-  valore: {
-    type: Number,
-    min: 0,
-    max: 100,
+  riordino: {
+    type: String,
+  },
+  yoga: {
+    type: String,
+  },
+  coding: {
+    type: String,
   },
 });
 
-const Task = mongoose.model("Task", taskSchema);
+const Streak = mongoose.model("Streak", streakSchema);
 
-const task = new Task({
-  giorno: 1,
-  valore: 50,
+const streak = new Streak({
+  day: "1",
+  riordino: "🦁",
+  yoga: "🧘🏼‍♂️",
+  coding: "👨‍💻",
 });
 
-//task.save();
+streak.save();
 
 app.get("/api/customers", (req, res) => {
-  Task.find({}, function (err, foundTasks) {
-    if (foundTasks.length === 0) {
+  Streak.find({}, function (err, foundStreaks) {
+    if (foundStreaks.length === 0) {
       console.log("Successfully zero");
     } else {
-      res.json(foundTasks);
+      res.json(foundStreaks);
     }
   });
 });
+
+// const taskSchema = new mongoose.Schema({
+//   giorno: {
+//     type: Number,
+//     min: 1,
+//     max: 31,
+//   },
+//   valore: {
+//     type: Number,
+//     min: 0,
+//     max: 100,
+//   },
+// });
+
+// const Task = mongoose.model("Task", taskSchema);
+
+// const task = new Task({
+//   giorno: 1,
+//   valore: 50,
+// });
+
+// //task.save();
+
+// app.get("/api/customers", (req, res) => {
+//   Task.find({}, function (err, foundTasks) {
+//     if (foundTasks.length === 0) {
+//       console.log("Successfully zero");
+//     } else {
+//       res.json(foundTasks);
+//     }
+//   });
+// });
 
 // });
 
